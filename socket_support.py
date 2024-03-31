9fcc93e6008e8bbf31139ff2f700bdbb76f94ee4
Pippin555 <harm.salomons@gmail.com>
2024-03-31 13:21:49 +0200

    inserted the socket support

---
+++ socket_support.py
@@ -0,0 +1,178 @@
+#! python3.12
+# coding: utf8
+
+""" connect to the server using sockets """
+
+__author__ = 'Sihir'
+__copyright__ = '© Sihir 2024-2024 all rights reserved'
+
+import socket
+import time
+
+from typing import Optional
+from typing import Callable
+
+
+class SocketClient:
+    """ simplest socket client"""
+
+    def __init__(self, port: int):
+        self._port = port
+
+    def connect_to_server(self) -> Optional[socket]:
+        """ Create a TCP/IP socket """
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Set a timeout for the connection attempt
+        # client_socket.settimeout(1)  # Timeout of 1 second
+
+        # Connect the socket to the server address and port
+        server_address = ('localhost', self._port)
+
+        try:
+            client_socket.connect(server_address)
+            # print('Connected to server:', server_address)
+            return client_socket
+
+        except socket.error as _:
+            # print('Error connecting to server:', e)
+            pass
+
+        return None
+
+    def communicate(self, message: str) -> Optional[str]:
+        """ establish a socket connection and send a message, return the answer """
+
+        outer = True
+        while outer:
+            client_socket = self.connect_to_server()
+            if client_socket is None:
+                # Wait for a second before attempting to reconnect
+                time.sleep(1)
+                continue
+
+            try:
+                while outer:
+                    try:
+                        client_socket.sendall(message.encode())
+                    except socket.error as e:
+                        print('Error sending data:', e)
+                        break  # Exit the loop if an error occurs
+
+                    # Receive response from the server
+                    try:
+                        data = client_socket.recv(1024)
+                    except socket.timeout:
+                        print('No response received within timeout period')
+                        continue  # Continue waiting for user input
+                    except socket.error as e:
+                        print('Error receiving data:', e)
+                        break  # Exit the loop if an error occurs
+
+                    if not data:
+                        print('Connection closed by server')
+                        break  # Exit the loop if server closes the connection
+
+                    return data.decode()
+
+            finally:
+                pass
+
+            # Close the connection
+            print('client: close the socket')
+            client_socket.close()
+
+        return None
+
+
+class SocketServer:
+    """ simplest socket server """
+
+    def __init__(self, port: int, callback: Callable, debug: bool):
+        """ initialize the class"""
+
+        self._port = port
+        self._callback = callback
+        self._debug = debug
+
+    def listen(self):
+        """ listen to TCP messages on the configured port """
+
+        connection = None
+        # Create a TCP/IP socket
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the address and port
+        server_address = ('localhost', self._port)
+        server_socket.bind(server_address)
+
+        # Listen for incoming connections
+        server_socket.listen(1)
+
+        if self._debug:
+            print('Server is listening...')
+
+        busy = True
+        while busy:
+            # Wait for a connection
+            try:
+                connection, client_address = server_socket.accept()
+                if self._debug:
+                    print('Connection from', client_address)
+
+            except socket.error as e:
+                if self._debug:
+                    print('Error accepting connection:', e)
+                continue  # Continue listening for new connections
+
+            try:
+                while busy:
+                    # Set a timeout for receiving data
+                    connection.settimeout(10)  # 10 seconds timeout (adjust as needed)
+                    answer = ''
+                    # Receive data from the client
+                    try:
+                        data = connection.recv(1024)
+
+                    except socket.timeout:
+                        if self._debug:
+                            print('No data received within timeout period')
+                        break  # Break out of the inner loop if timeout occurs
+
+                    except socket.error as e:
+                        if self._debug:
+                            print('Error receiving data:', e)
+                        break  # Break out of the inner loop if an error occurs
+
+                    if not data:
+                        break  # Break out of the inner loop if no data is received
+
+                    message = data.decode()
+                    busy, answer = self.check_message(message)
+
+                    # Send a response back to the client
+                    connection.sendall(answer.encode())
+            finally:
+                pass
+
+        if connection is not None:
+            # Close the connection
+            print('server: close the connection')
+            connection.close()
+
+    def check_message(self, message: str):
+        """ check the message, get an answer from the callback """
+
+        busy = message != '[QUIT]'
+        if busy:
+            if message and self._callback:
+                answer = self._callback(message)
+            else:
+                answer = 'ignored'
+        else:
+            answer = 'Server is halting'
+            if self._debug:
+                print(answer)
+
+        print(f'busy: {busy}')
+        return busy, answer
--- web_engine.py
+++ web_engine.py
@@ -9,47 +9,79 @@ __copyright__ = '© Sihir 2023-2023 all rights reserved'  # noqa
 from sys import exit as _exit
 from sys import argv
 
+from os import curdir
+
 from os.path import abspath
 from os.path import isfile
-from os.path import splitext
-from os.path import basename
+from os.path import join
+
+from threading import Thread
+from typing import Optional
 
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
 from PySide6.QtCore import QRect
 
+from socket_support import SocketServer
 
-def display_html(html_file):
 
-    app = QApplication(argv)
-    web_view = QWebEngineView()
+class HelpViewer:
 
-    title, _ = splitext(basename(html_file))
-    web_view.setWindowTitle(title.capitalize())
+    def __init__(self, port: int = 4995):
+        """ ... """
 
-    web_view.setGeometry(QRect(100, 100, 1200, 800))
-    web_view.load(QUrl.fromLocalFile(html_file))
-    web_view.show()
-    _exit(app.exec())
+        self.thread = Thread(target=self.socket_thread)
+        self.server = SocketServer(port=port, callback=self.change_page, debug=True)
+
+    def socket_thread(self):
+        """ ... """
+        self.server.listen()
+
+    def display_subject(self, subject):
+        """ ... """
+
+        self.app = QApplication(argv)
+        self.web_view = QWebEngineView()
+        self.web_view.setWindowTitle(subject.capitalize())
+
+        self.web_view.setGeometry(QRect(100, 100, 1200, 800))
+        file = self.html_file(subject=subject)
+        if file is not None:
+            self.web_view.load(QUrl.fromLocalFile(file))
+        self.web_view.show()
+        _exit(self.app.exec())
+
+    def html_file(self, subject: str) -> Optional[str]:
+        """ ... """
+
+        file = abspath(join(curdir, 'help', f'{subject}.html'))
+        if not isfile(file):
+            print(f'subject not found {subject}')
+            return None
+        return file
+
+    def change_page(self, subject: str):
+        """ ... """
+
+        file = self.html_file(subject=subject)
+        if file is not None:
+            self.web_view.load(QUrl.fromLocalFile(file))
 
 
 def main() -> int:
     """ main entry """
 
-    html_file = None
+    subject = None
     it_arg = iter(argv[1:])
 
     for arg in it_arg:
         match arg:
             case '-h':
-                html_file = abspath(next(it_arg))
+                subject = next(it_arg)
 
-    if not isfile(html_file):
-        print(f'file not found {html_file}')
-        return 1
-
-    display_html(html_file)
+    viewer = HelpViewer(port=4995)
+    viewer.display_subject(subject)
     return 0
 
 
